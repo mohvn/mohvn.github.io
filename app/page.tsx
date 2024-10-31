4df5a4cae5dc7c5e70b3d7d528fa5e40e1e0b6af
Mohan Elias <mohan.elias@proton.me>
2024-10-31 15:06:10 -0300

    feat: hover color

--- app/page.tsx
+++ app/page.tsx
@@ -8,29 +8,31 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-jetbrains-mono)] text-[#c9c7cd]">
-      <h3 className="text-sm text-[#484848] absolute top-0 left-5 p-1 bg-background">header</h3>
-      <header className="flex items-center justify-end p-4 border m-4 border-[#484848]">
-        <div className="flex items-center gap-2">
-          <p>
-            <span className="font-bold">0x</span>
-            <span>9BA6ADCC0F05BE4B</span>
-          </p>
-          <p className="text-xl text-[#ffc799]">[</p>
-          <a href="https://www.linkedin.com/in/mohaneliasv/">
-            <RiLinkedinFill fill="currentColor" className="w-6 h-6 hover:text-[#ffc799]" />
-          </a>
-          <p className="text-xl text-[#ffc799]">,</p>
-          <a href="https://github.com/mohvn">
-            <CibGithub fill="currentColor" className="w-6 h-6 hover:text-[#ffc799]" />
-          </a>
-          <p className="text-xl text-[#ffc799]">,</p>
-          <a href="mailto:mohvn.elias@hotmail.com">
-            <IcBaselineEmail fill="currentColor" className="w-6 h-6 hover:text-[#ffc799]" />
-          </a>
-          <p className="text-xl text-[#ffc799]">]</p>
-          <ModeToggle />
-        </div>
-      </header>
+      <div className="group">
+        <h3 className="text-sm text-[#484848] absolute top-0 left-5 p-1 bg-background group-hover:text-[#ffc799] transition-all duration-300">header</h3>
+        <header className="flex items-center justify-end p-4 border m-4 border-[#484848] group-hover:border-[#ffc799] transition-all duration-300">
+          <div className="flex items-center gap-2">
+            <p>
+              <span className="font-bold">0x</span>
+              <span>9BA6ADCC0F05BE4B</span>
+            </p>
+            <p className="text-xl text-[#ffc799]">[</p>
+            <a href="https://www.linkedin.com/in/mohaneliasv/">
+              <RiLinkedinFill fill="currentColor" className="w-6 h-6 hover:text-[#ffc799]" />
+            </a>
+            <p className="text-xl text-[#ffc799]">,</p>
+            <a href="https://github.com/mohvn">
+              <CibGithub fill="currentColor" className="w-6 h-6 hover:text-[#ffc799]" />
+            </a>
+            <p className="text-xl text-[#ffc799]">,</p>
+            <a href="mailto:mohvn.elias@hotmail.com">
+              <IcBaselineEmail fill="currentColor" className="w-6 h-6 hover:text-[#ffc799]" />
+            </a>
+            <p className="text-xl text-[#ffc799]">]</p>
+            <ModeToggle />
+          </div>
+        </header>
+      </div>
       <h3 className="text-sm text-[#484848] absolute top-[87px] left-5 p-1 bg-background">content</h3>
       <div className="flex justify-center mx-4 gap-4 border p-4 border-[#484848]">
         <div>
